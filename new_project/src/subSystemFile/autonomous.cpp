37b999bd2e1a0fa192a67db604ae49700692a47d
lghuy05 <luonggiahuy1909@gmail.com>
2025-12-12 23:30:54 -0500

    add

--- new_project/src/subSystemFile/autonomous.cpp
+++ new_project/src/subSystemFile/autonomous.cpp
@@ -374,11 +374,11 @@ void myAutonomous() {
   timedDrive(30, 2000);
   timedTurn(-30, 1400);
   togglePiston();
-
   intake_motor.move(-100);
   intake_motor2.move(-100);
   intake_motor3.move(100);
   pros::delay(3000);
+  piston1.set_value(false);
 
   // 2. Slow down AND run intake SIMULTANEOUSLY for 1 second (timed)
   // pros::lcd::print(0, "Grabbing ball");
