c31bbfd2c3a6448f6ac98bcccb8c6565e2cc4f8a
lghuy05 <luonggiahuy1909@gmail.com>
2025-12-13 10:16:56 -0500

    flipped

--- new_project/src/subSystemFile/autonomous.cpp
+++ new_project/src/subSystemFile/autonomous.cpp
@@ -384,6 +384,34 @@ void myAutonomous() {
   intake_run();
   pros::delay(3000);
   piston1.set_value(false);
+
+  // reversed
+
+  timedTurn(26, 882);
+  timedDrive(33, 3700);
+  timedTurn(-30, 1600);
+
+  leftDrive.move(20);
+  rightDrive.move(20);
+  pros::delay(1400);
+  leftDrive.move(20);
+  rightDrive.move(20);
+  intake_motor.move(-100);
+  intake_motor2.move(-100);
+  pros::delay(2000);
+  //
+  // leftDrive.move(0);
+  // rightDrive.move(0);
+  // intake_motor.move(0);
+  // intake_motor2.move(0);
+  timedTurn(-25, 600);
+  timedDrive(30, 2000);
+  timedTurn(-30, 1700);
+  timedDrive(20, 500);
+  togglePiston();
+  intake_run();
+  pros::delay(3000);
+  piston1.set_value(false);
   //
   // 2. Slow down AND run intake SIMULTANEOUSLY for 1 second (timed)
   // pros::lcd::print(0, "Grabbing ball");
